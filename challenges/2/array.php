26fd18fc9bc45d6dba250001849fdfa1dde491ab
celularbeing <ivan5247@gmail.com>
2016-08-14 17:36:25 -0700

    More Changes
    List styling changed. Added comments.

--- challenges/2/array.php
+++ challenges/2/array.php
@@ -5,26 +5,32 @@
 	 */
 	 
 	$post_num = substr($_SERVER[REQUEST_URI], 36);//substrings my file directory past '?'
-
-	$myList = array();
+	
+	$myList = array(); //storing fib values
 	$first = 0;
 	$second = 1;
 	array_push($myList, $first);
 	array_push($myList, $second);
 	
-	for($i = 2; $i < $post_num; $i++){
+	for($i = 2; $i <= $post_num; $i++){ //iterate until nth value
 		$third = $first + $second;
 		array_push($myList, $third);
 		$first = $second;
 		$second = $third;
 	}	
 	
- 	$theData = array('status' => http_response_code(),
-				  'success' => "true",
+	$statusCode = http_response_code();
+	$success = false;
+	if($statusCode == 200){
+		$success = true;
+	}
+	
+ 	$theData = array('status' => $statusCode,
+				  'success' => $success,
 				  'version' => "JSON-Array-0.1",
 				  'Fibonacci' => $post_num,
-				  'numbers' => $myList
+				  'numbers' => (object)$myList
 	); 
 
-	echo json_encode($theData, JSON_PRETTY_PRINT);
+	echo json_encode($theData, JSON_PRETTY_PRINT)."\n"; //JSON print
 ?>
